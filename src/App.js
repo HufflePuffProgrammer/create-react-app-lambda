3e6897f0a61d63911ba3895a589b670540a81d3f
hufflepuffprogrammer <wdrum4food@yahoo.com>
2020-04-26 21:45:55 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+import { gql } from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+import ApolloClient from "apollo-boost";
+
+// Replace the previous LambdaDemo with the code below:
+// const client = new ApolloClient({
+//   uri: "/.netlify/functions/graphql",
+// });
+const client = new ApolloClient({
+  uri: "http://localhost:3000/.netlify/functions/graphql",
+});
+
+const APOLLO_QUERY = gql`
+  {
+    hello
   }
+`;
+const LambdaDemo = () => (
+  <ApolloProvider client={client}>
+    <Query query={APOLLO_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading) return <span>Loading...</span>;
+        if (error) return <p>Error: {error.message}</p>;
+        return <div>A greeting from the server: {data.hello}</div>;
+      }}
+    </Query>
+  </ApolloProvider>
+);
 
-  handleClick = api => e => {
-    e.preventDefault()
+// class LambdaDemo extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = { loading: false, msg: null }
+//   }
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+//   handleClick = api => e => {
+//     e.preventDefault()
 
-  render() {
-    const { loading, msg } = this.state
+//     this.setState({ loading: true })
+//     fetch("/.netlify/functions/" + api)
+//       .then(response => response.json())
+//       .then(json => this.setState({ loading: false, msg: json.msg }))
+//   }
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+//   render() {
+//     const { loading, msg } = this.state
+
+//     return (
+//       <p>
+//         <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
+//         <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+//         <br />
+//         <span>{msg}</span>
+//       </p>
+//     )
+//   }
+// }
 
 class App extends Component {
   render() {
@@ -40,11 +69,12 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          4
           <LambdaDemo />
         </header>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
